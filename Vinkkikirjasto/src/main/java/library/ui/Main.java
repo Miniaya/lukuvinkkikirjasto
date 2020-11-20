3e082a9c26831270fc9cddf0e433d87b00f25ce3
aleksinuora <aleksinuora@gmail.com>
2020-11-20 12:15:27 +0200

    .

--- Vinkkikirjasto/src/main/java/library/ui/Main.java
+++ Vinkkikirjasto/src/main/java/library/ui/Main.java
@@ -2,11 +2,8 @@ package library.ui;
 
 import library.domain.LibraryService;
 import java.util.Scanner;
-<<<<<<< HEAD
 import java.io.PrintWriter;
-=======
 import library.dao.SQLLibraryDao;
->>>>>>> d6cefcce677fe7399a1aa8bb897018df55f4ef0f
 
 public class Main {
     
@@ -24,7 +21,7 @@ public class Main {
             in = new Scanner(System.console().reader());
 	} catch (NullPointerException e) {
             // Virhetilanteessa käytetään tavallista tapaa.
-            in = new Scanner(System.in);
+            in = new Scanner(System.in, "Cp850");
 	}
     }
     // </ÄÄKKÖSKORJAUS> 
@@ -32,14 +29,8 @@ public class Main {
     
     public static void main(String[] args) {
         // pääohjelma, tämä käyttää vain LibraryServicen metodeja!
-        
-<<<<<<< HEAD
 //        Scanner scanner = new Scanner(System.in, "Cp850");
-        CLUI ui = new CLUI(in);
-=======
-        Scanner scanner = new Scanner(System.in, "Cp850");
-        CLUI ui = new CLUI(scanner, new SQLLibraryDao("jdbc:sqlite:vinkit.db", "vinkit.db"));
->>>>>>> d6cefcce677fe7399a1aa8bb897018df55f4ef0f
+        CLUI ui = new CLUI(in, new SQLLibraryDao("jdbc:sqlite:vinkit.db", "vinkit.db"));
         ui.init();
     }
 }
