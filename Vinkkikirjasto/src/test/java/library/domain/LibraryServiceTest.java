b1d4cadd362d356ecf78bf575fa86a6b01361ce8
sinisaarinen <sini.saarinen@hotmail.fi>
2020-12-04 20:00:35 +0200

    added a test to class LibraryServiceTest

--- Vinkkikirjasto/src/test/java/library/domain/LibraryServiceTest.java
+++ Vinkkikirjasto/src/test/java/library/domain/LibraryServiceTest.java
@@ -3,8 +3,6 @@ package library.domain;
 import java.util.ArrayList;
 import library.dao.InMemoryLibraryDao;
 import library.dao.LibraryDao;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -13,19 +11,31 @@ public class LibraryServiceTest {
     
     private LibraryDao dao;
     private LibraryService service;
+    private String[] bookDetails;
+    private ArrayList<String> bookInput;
+    private String[] articleDetails;
+    private ArrayList<String> articleInput;
     
     @Before
     public void setUp() {
         dao = new InMemoryLibraryDao();
         service = new LibraryService(dao);
-    }
-    
-    @After
-    public void tearDown() {
+        bookDetails = new String[]{"nimi", "kirjoittaja", "sivumäärä", "tagit"};
+        bookInput = new ArrayList<>();
+        articleDetails = new String[]{"nimi", "url", "tagit"};
+        articleInput = new ArrayList<>();
     }
     
     @Test
-    public void addingValidBookReturnsTrue() {
+    public void addingValidBookOrArticleReturnsTrue() {
+        bookInput.add("TestBook");
+        bookInput.add("TestAuthor");
+        bookInput.add("100");
+        bookInput.add("TestTag");
+        assertTrue(service.add("book", bookDetails, bookInput.toArray(new String[bookInput.size()])));
+        articleInput.add("TestArticle");
+        articleInput.add("TestUrl");
+        articleInput.add("TestTag");
+        assertTrue(service.add("article", articleDetails, articleInput.toArray(new String[articleInput.size()])));
     }
-    
 }
