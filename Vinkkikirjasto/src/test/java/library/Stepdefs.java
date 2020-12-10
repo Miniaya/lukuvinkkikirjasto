a9605583da85d92860df2766bcc18cb066fbd887
Sonja Salmi <salmi.sonjakaroliina@gmail.com>
2020-12-10 21:07:28 +0200

    toinen cucumbertesti; stepdefs:n siistimistä

--- Vinkkikirjasto/src/test/java/library/Stepdefs.java
+++ Vinkkikirjasto/src/test/java/library/Stepdefs.java
@@ -58,6 +58,13 @@ public class Stepdefs {
                 new String[]{m.getName(), m.getAuthor(), Integer.toString(m.getPages()), Double.toString(m.getRead()), m.getTags()});
     }
     
+    @Given ("article Ohtumateriaali with tag ohjelmistotuotanto is saved")
+    public void saveOhtuArticle() {
+        Article a = this.getArticleOhtumateriaali();
+        library.add("article", a.getDetailTypes(), 
+                new String[]{a.getName(), a.getUrl(), a.getTags()});
+    }
+    
     @Given ("command hae is selected")
     public void commandHaeIsSelected() {
         input.add("hae");
@@ -111,42 +118,15 @@ public class Stepdefs {
         input.add(tag);
     }
 
-    @Then("book is saved to library")
-    public void bookIsSaved() {
-        input.add("sulje");
-
-        io.setInputs(input);
-        ui.init();
-        assertTrue(io.getPrints().contains("Vinkki lisätty."));
+    @When("name of the book {string} and amount of pages read {string} is given")
+    public void nameOfTheBookAndAmountOfPagesReadIsGiven(String string, String string2) {
+        input.add("muokkaa");
+        input.add("sivumaara");
+        input.add(string);
+        input.add(string2);
     }
-
-    @Then("error message {string} is shown")
-    public void errorMessageShown(String message) {
-        input.add("sulje");
-
-        io.setInputs(input);
-        ui.init();
-        assertTrue(io.getPrints().contains(message));
-    }
-
-    @Then("book is removed from library")
-    public void bookIsRemoved() {
-        input.add("sulje");
-
-        io.setInputs(input);
-        ui.init();
-        assertTrue(io.getPrints().contains("Kirja kirja poistettu vinkkikirjastosta."));
-    }
-
-    @Then("article is removed from library")
-    public void articleIsRemoved() {
-        input.add("sulje");
-
-        io.setInputs(input);
-        ui.init();
-        assertTrue(io.getPrints().contains("Artikkeli artikkeli poistettu vinkkikirjastosta."));
-    }
-
+    
+    
     @When("correct article information is given")
     public void correctArticleInformationIsGiven() {
         input.add("artikkeli");
@@ -155,67 +135,61 @@ public class Stepdefs {
         input.add("Nonsense");
     }
 
-    @Then("article is saved to library")
-    public void articleIsSavedToLibrary() {
-        input.add("sulje");
-
-        io.setInputs(input);
-        ui.init();
+    
+    @Then("book is saved to library")
+    public void bookIsSaved() {
+        this.closeAndRunCLUI();
         assertTrue(io.getPrints().contains("Vinkki lisätty."));
     }
 
-    @When("name of the book {string} and amount of pages read {string} is given")
-    public void nameOfTheBookAndAmountOfPagesReadIsGiven(String string, String string2) {
-        input.add("muokkaa");
-        input.add("sivumaara");
-        input.add(string);
-        input.add(string2);
+    @Then("error message {string} is shown")
+    public void errorMessageShown(String message) {
+        this.closeAndRunCLUI();
+        assertTrue(io.getPrints().contains(message));
     }
+
+    @Then("book is removed from library")
+    public void bookIsRemoved() {
+        this.closeAndRunCLUI();
+        assertTrue(io.getPrints().contains("Kirja kirja poistettu vinkkikirjastosta."));
+    }
+
+    @Then("article is removed from library")
+    public void articleIsRemoved() {
+        this.closeAndRunCLUI();
+        assertTrue(io.getPrints().contains("Artikkeli artikkeli poistettu vinkkikirjastosta."));
+    }
+
+    @Then("article is saved to library")
+    public void articleIsSavedToLibrary() {
+        this.closeAndRunCLUI();
+        assertTrue(io.getPrints().contains("Vinkki lisätty."));
+    }
+
 
     @Then("pages read is updated with the new value")
     public void pagesReadIsUpdatedWithTheNewValue() {
-        input.add("sulje");
-        io.setInputs(input);
-        ui.init();
+        this.closeAndRunCLUI();
         assertTrue(io.getPrints().contains("Luettu sivumäärä päivitetty."));
     }
     
     @Then("number of pages read are displayed in red")
     public void pagesDisplayedRed() {
-        input.add("sulje");
-        
-        io.setInputs(input);
-        ui.init();
-        boolean result = false;
-        for (String s: io.getPrints()) {
-            if (s.contains("\nLuettu     | \u001b[38;5;210m")) {
-                result = true;
-            }
-        }
+        this.closeAndRunCLUI();
+        boolean result = this.goThroughPrintsLookingFor("\nLuettu     | \u001b[38;5;210m");
         assertTrue(result);
     }
     
     @Then("number of pages read are displayed in yellow")
     public void pagesDisplayedYellow() {
-        input.add("sulje");
-        
-        io.setInputs(input);
-        ui.init();
-        boolean result = false;
-        for (String s: io.getPrints()) {
-            if (s.contains("\nLuettu     | \u001b[38;5;229m")) {
-                result = true;
-            }
-        }
+        this.closeAndRunCLUI();
+        boolean result = this.goThroughPrintsLookingFor("\nLuettu     | \u001b[38;5;229m");
         assertTrue(result);
     }
     
     @Then("number of pages read are displayed in green")
     public void pagesDisplayedGreen() {
-        input.add("sulje");
-        
-        io.setInputs(input);
-        ui.init();
+        this.closeAndRunCLUI();
         boolean result = false;
         // BUGI: jostain syystä tätä testiä ei saa toimimaan vaikka se on
         // periaatteessa täsmälleen samanlainen kuin testit punaiselle ja
@@ -230,21 +204,42 @@ public class Stepdefs {
     
     @Then ("info for Margarita is displayed")
     public void margaritaInfoDisplayed() {
+        this.closeAndRunCLUI();
+        Book book = this.getBookMargarita();
+        Boolean result = this.goThroughPrintsLookingFor(book.toString());
+        assertTrue(result);
+    }
+    
+    @Then ("info for Ohtumateriaali is displayed")
+    public void ohtuInfoDisplayed() {
+        this.closeAndRunCLUI();
+        Article ohtu = this.getArticleOhtumateriaali();
+        boolean result = this.goThroughPrintsLookingFor(ohtu.toString());
+        assertTrue(result);
+    }
+    
+    private void closeAndRunCLUI() {
         input.add("sulje");
         io.setInputs(input);
-        ui.init();
-        Book book = this.getBookMargarita();
-        boolean result = false;
+        ui.init(); 
+    }
+    
+    private boolean goThroughPrintsLookingFor(String toFind) {
         for (String s: io.getPrints()) {
-            if (s.contains(book.toString())) {
-                result = true;
+            if (s.contains(toFind)) {
+                return true;
             }
         }
-        assertTrue(result);
+        return false;
     }
     
     private Book getBookMargarita() {
         Book book = new Book("Margarita", "Anni Kytömäki", 581, 0, "finlandia");
         return book;
+    }
+    
+    private Article getArticleOhtumateriaali() {
+        Article article = new Article("Ohtumateriaali", "https://ohjelmistotuotanto-hy.github.io/", "ohjelmistotuotanto");
+        return article;
     }
 }
--- Vinkkikirjasto/src/test/resources/library/searchByTag.feature
+++ Vinkkikirjasto/src/test/resources/library/searchByTag.feature
@@ -5,3 +5,9 @@ Feature: As a user I want to be able to search suggestions by tag
         And command hae is selected
         When tag "finlandia" is entered
         Then info for Margarita is displayed
+
+    Scenario: When there is an article tagges, search returns it
+        Given article Ohtumateriaali with tag ohjelmistotuotanto is saved
+        And command hae is selected
+        When tag "ohjelmistotuotanto" is entered
+        Then info for Ohtumateriaali is displayed
